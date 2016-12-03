ae69ecc0f0c3a702e93002ad0d0c39aef3578f62
TaMenServer <tamenserver@gmail.com>
2016-12-03 10:47:13 +0800

    Update

--- src/com/tamenserver/standard/setinterpreter/SetInterpreter.java
+++ src/com/tamenserver/standard/setinterpreter/SetInterpreter.java
@@ -1,5 +1,14 @@
 package com.tamenserver.standard.setinterpreter;
 
-public class SetInterpreter {
+import java.util.Arrays;
+import java.util.List;
 
+public class SetInterpreter {
+    static List<String> setInterpreter(String set) {
+        if (set.charAt(set.length() - 2) == ',') {
+            return Arrays.asList(set.substring(1, set.length() - 3).split(","));
+        } else {
+            return Arrays.asList(set.substring(1, set.length() - 3).split(","));
+        }
+    }
 }
