c71b2a02608a6f692749eed68717cbf82a99f628
TaMenServer <tamenserver@gmail.com>
2016-12-03 11:42:19 +0800

    Update

---
+++ src/com/tamenserver/standard/setinterperter/exception/NotASetException.java
@@ -0,0 +1,6 @@
+package com.tamenserver.standard.setinterperter.exception;
+
+public class NotASetException extends Exception {
+
+    private static final long serialVersionUID = 4232273163782929814L;
+}
--- src/com/tamenserver/standard/setinterpreter/SetInterpreter.java
+++ src/com/tamenserver/standard/setinterpreter/SetInterpreter.java
@@ -3,12 +3,24 @@ package com.tamenserver.standard.setinterpreter;
 import java.util.Arrays;
 import java.util.List;
 
+import com.tamenserver.standard.setinterperter.exception.NotASetException;
+
 public class SetInterpreter {
-    static List<String> setInterpreter(String set) {
-        if (set.charAt(set.length() - 2) == ',') {
-            return Arrays.asList(set.substring(1, set.length() - 3).split(","));
+
+    public List<String> setInterpreter(String set) throws NotASetException {
+        for (int i = 1; i < set.length() - 1; i++) {
+            if (set.charAt(i) == '{' || set.charAt(i) == '}') {
+                throw new NotASetException();
+            }
+        }
+        if (set.charAt(0) == '{' && set.charAt(set.length() - 1) == '}') {
+            if (set.charAt(set.length() - 2) == ',') {
+                return Arrays.asList(set.substring(1, set.length() - 2).split(","));
+            } else {
+                return Arrays.asList(set.substring(1, set.length() - 1).split(","));
+            }
         } else {
-            return Arrays.asList(set.substring(1, set.length() - 3).split(","));
+            throw new NotASetException();
         }
     }
 }
